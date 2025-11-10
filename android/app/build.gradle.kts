82293ef5136d82096f220969762ca7c1ae04ad73
Alex727727 <jks6598@psu.edu>
2025-11-10 03:48:56 -0500

    android 출시 버전의 코드

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
 }
 
 android {
-    namespace = "com.example.tago_driver"
+    namespace = "com.tagodriver.ploride"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -50,7 +50,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tago_driver"
+        applicationId = "com.tagodriver.ploride"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
